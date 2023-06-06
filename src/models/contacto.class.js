68a13bca0ed09ea63849d421b6d7186a646597b3
matubruno16 <matubruno16@gmail.com>
2023-06-06 16:14:31 -0300

    updated

---
+++ src/models/contacto.class.js
@@ -0,0 +1,16 @@
+
+
+export class Contacto {
+    name = '';
+    apellido = '';
+    email = '';
+    conectado = false;
+
+    constructor (nombre, apellido, email ,conectado) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.conectado = conectado;
+    }
+}
+
